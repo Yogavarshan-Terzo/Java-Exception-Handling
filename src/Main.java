721879b1bb9ab22715fd21256ac2d7560160155c
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-15 17:35:23 +0530

    Implement-Multiple-Exception

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
             var reader = new FileReader("file.txt");
+            var value = reader.read();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        catch (IOException e){
+            System.out.println("Could not read input");        }
     }
 }
